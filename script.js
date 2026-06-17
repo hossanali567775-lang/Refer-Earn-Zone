362013cb92956f02febd170635fb3725be66a06a
hossanali567775-lang <hossanali567775@gmail.com>
2026-06-17 20:23:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-const firebaseConfig = { apiKey: "AIzaSyA0iU2q_AVn78ICFMjbISJKXhZYkuph4Cw", databaseURL: "https://refer-earn-zone-default-rtdb.firebaseio.com", projectId: "refer-earn-zone" };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+console.log("Earn Zone Loaded");
 
-function switchTab(id) {
-    document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
-    document.getElementById(id).classList.add('active');
-}
+let balance = 0;
+let referrals = 0;
 
-// এখানে আপনার বাটন ক্লিক ও ব্যালেন্স আপডেটের লজিকগুলো লিখুন...
-// উদাহরণস্বরূপ:
-function copyReferLink() {
-    navigator.clipboard.writeText("https://t.me/bdfreeincometkBot/app?start=6763447817");
-    alert("লিংক কপি হয়েছে!");
-}
-
-function requestWithdraw() {
-    alert("উইথড্র রিকোয়েস্ট পাঠানো হয়েছে!");
+function showMessage(msg){
+    alert(msg);
 }
