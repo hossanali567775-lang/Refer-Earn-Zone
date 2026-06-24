7fb436baa97c6a5fe75b3f709d7aed578dfaa3c7
hossanali567775-lang <hossanali567775@gmail.com>
2026-06-24 16:48:49 +0600

    Refactor Firebase config and enhance user balance display
    Updated Firebase configuration and added totalRef display.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0iU2q_AVn78ICFMjbISJKXhZYkuph4Cw",
-  authDomain: "refer-earn-zone.firebaseapp.com",
-  databaseURL: "https://refer-earn-zone-default-rtdb.firebaseio.com",
-  projectId: "refer-earn-zone",
-  storageBucket: "refer-earn-zone.firebasestorage.app",
-  messagingSenderId: "1035860948377",
-  appId: "1:1035860948377:web:5fc19e6fcc844cec6e4f08",
-  measurementId: "G-WWCVYZQY9P"
+apiKey: "AIzaSyA0iU2q_AVn78ICFMjbISJKXhZYkuph4Cw",
+authDomain: "refer-earn-zone.firebaseapp.com",
+databaseURL: "https://refer-earn-zone-default-rtdb.firebaseio.com",
+projectId: "refer-earn-zone",
+storageBucket: "refer-earn-zone.firebasestorage.app",
+messagingSenderId: "1035860948377",
+appId: "1:1035860948377:web:5fc19e6fcc844cec6e4f08",
+measurementId: "G-WWCVYZQY9P"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,13 +19,11 @@ const db = getDatabase(app);
 const userId = localStorage.getItem("userId") || Date.now();
 localStorage.setItem("userId", userId);
 
-document.getElementById("username").innerText = "ABIR";
-document.getElementById("userid").innerText = "User ID: " + userId;
-
 // Balance
 let balance = localStorage.getItem("balance") || 0;
 document.getElementById("balance").innerText = balance;
-
+let totalRef = localStorage.getItem("totalRef") || 0;
+document.getElementById("totalRef").innerText = totalRef;
 // Referral Link
 const refLink =
 window.location.origin +
@@ -36,7 +34,7 @@ userId;
 document.getElementById("refLink").innerText = refLink;
 
 // Copy Button
-const copyBtn = document.querySelector(".card button");
+const copyBtn = document.getElementById("copyRefBtn");
 
 if(copyBtn){
 copyBtn.onclick = () => {
